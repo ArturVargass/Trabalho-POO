4acb96931ce80dd27fb528523e5abcae0a65332d
adriansgpy <95186647+adriansgpy@users.noreply.github.com>
2024-11-14 09:19:51 -0300

    Batalha e round funcionando

--- Batalha.java
+++ Batalha.java
@@ -1,54 +1,38 @@
 public class Batalha {
 
     private Bolla bolla;
+
     private Bolla inimigo;
 
+    public Batalha(Bolla bolla, Bolla inimigo){
 
-
-    public Batalha(Bolla bolla, Bolla b2){
         this.bolla = bolla;
-        this.inimigo = b2;
+
+        this.inimigo = inimigo;
+
     }
 
-    /*public void iniciarBatalha(){
+    public void iniciarBatalha(){
 
-        Round round = new Round();
+        Round round = new Round(bolla, inimigo);
 
-        while(checarBatalha() == null){
+        while(round.finalizarBatalha() == 2){
+
+            round.realizarRound();
 
         }
 
-    }*/
 
-    public void finalizarBatalha(){
-        //mostrar na interface que a batalha foi encerrada
+
     }
 
-    public void setVitoria(Treinador vencedor, Treinador perdedor){
+    public void setResultado(Treinador vencedor){
+
         int v = vencedor.getVitorias();
+
         vencedor.setVitorias(v + 1);
-        int d = perdedor.getDerrotas();
-        perdedor.setDerrotas(d + 1);
+
     }
-
-   /* public Bolla checarBatalha(){
-
-        if(this.b1.getHp() == 0 || this.b1.getHp() < 0){
-
-            finalizarBatalha();
-            return inimigo;
-
-        }else if(this.inimigo.getHp() == 0 || this.inimigo.getHp() < 0){
-
-            finalizarBatalha();
-            return b1;
-
-       }
-
-       return null;
-
-    }*/
-
 
 
 }
--- Round.java
+++ Round.java
@@ -1,3 +1,46 @@
 public class Round {
 
+    private Bolla bolla, inimigo;
+
+    private int contador = 0;
+
+    public Round(Bolla bolla, Bolla inimigo){
+
+        this.bolla = bolla;
+
+        this.inimigo = inimigo;
+
+    }
+
+    public int realizarRound(){
+
+        bolla.atacarBasico(inimigo);
+        inimigo.atacarBasico(bolla);
+
+        System.out.println(" round "+ contador + "-------------------------");
+        System.out.println("bolla hp: "+bolla.getHp());
+        System.out.println("inimigo hp: "+inimigo.getHp());
+        System.out.println("-------------------------");
+
+        contador +=1;
+
+        return finalizarBatalha();
+
+    }
+
+    public int finalizarBatalha() {
+        if (bolla.getHp() <= 0) {
+
+            return 0;
+
+        } else if(inimigo.getHp() <= 0){
+
+            return 1;
+
+        }
+
+        return 2;
+
+    }
+
 }
