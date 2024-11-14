f0f03c56da3ea0371ab037db718e8c2b137ea256
ArturVargass <134974019+ArturVargass@users.noreply.github.com>
2024-11-14 11:42:09 -0300

    Add files via upload

---
+++ Zelrix.java
@@ -0,0 +1,71 @@
+import java.io.Serializable;
+import java.util.Random;
+
+    public class Zelrix extends Bolla implements Serializable {
+
+
+        public Zelrix() {
+            super(1, 200, 30, "imagens/Zelrix.png");
+        }
+
+
+        @Override
+        public void atacarBasico(Bolla b) {
+            Random random = new Random();
+            float dano;
+
+            if (random.nextFloat() < 0.35) {
+                dano = (float) (this.getPoder() * 2); // Dano crítico
+            } else {
+                dano =  this.getPoder(); // Dano normal
+            }
+
+            b.setHp(Math.max(0, b.getHp() - dano));
+        }
+
+        @Override
+        public void ataqueEspecial(Bolla b) {
+            Random random = new Random();
+
+            float danoBase = this.getPoder() + 15.0f;
+            float dano;
+
+            if (random.nextFloat() < 0.30) {
+
+                dano = danoBase * 2.5f;
+                this.setHp(this.getHp() + 25);
+
+            } else {
+
+                dano = 0;
+
+            }
+
+            b.setHp(Math.max(0, b.getHp() - dano));
+        }
+
+        @Override
+        public void atacar(Bolla b) {
+
+            Random random = new Random();
+
+            if (random.nextFloat() < 0.35) {
+
+                ataqueEspecial(b);
+
+            } else {
+
+                atacarBasico(b);
+
+            }
+
+        }
+
+        @Override
+        public void defender(float danoRecebido) {
+
+        }
+
+
+    }
+
